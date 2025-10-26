baecd2693ff2303b407e06c643a0fe62afdc6721
Twisted Brain <lars.peder.mathiasen@gmail.com>
2025-10-26 13:16:35 +0100

    auto commit - 2025-10-26 13:16:35

--- apps/front/src/routes/cards/scenes/new/+page.svelte
+++ apps/front/src/routes/cards/scenes/new/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import { SceneEditorView } from '@loke/apps-cards';
+  import { SceneCreateView } from '@loke/apps-cards/module';
 </script>
 
-<div class="p-6 max-w-4xl mx-auto">
-  <h1 class="text-3xl font-bold text-gray-900 dark:text-white mb-6">Create New Scene</h1>
-
-  <div class="bg-white dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-700 p-6">
-    <SceneEditorView sceneId={null} />
-  </div>
-</div>
+<SceneCreateView />
